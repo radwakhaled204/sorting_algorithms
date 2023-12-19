59f92b6d9717dcef7c5432dca823b5b4d763d00b
Ahmedsayed210 <romaahmed791@gmail.com>
2023-12-19 11:12:50 -0800

    task0 changes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,13 +21,16 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j + 1] < array[j])
 			{
-				swap(&array[j + 1], &array[j]);
+				swap(array + j, array + j + 1);
 				print_array(array, size);
 			}
 		}
