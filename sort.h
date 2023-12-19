06ac2825f425df3171060009a1cf4423f86e8b00
Radwa Khaled <131785821+Radwa-Khaled@users.noreply.github.com>
2023-12-19 21:31:26 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -3,6 +3,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 /**
  * struct listint_s - Doubly linked list node
  *
