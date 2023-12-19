154020e1a2c512fb118107b351bc8e7b12897866
Ahmedsayed210 <romaahmed791@gmail.com>
2023-12-19 10:36:13 -0800

    task0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * swap - Swaps values pointed to by integer pointers.
+ *
+ * @a: Pointer to the first integer variable.
+ * @b: Pointer to the second integer variable.
+ */
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+/**
+ * bubble_sort - Bubble sort for integers, prints array after each swap.
+ *
+ * @array: Pointer to the integer array to be sorted.
+ * @size: Size of the array.
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j + 1] < array[j])
+			{
+				swap(&array[j + 1], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+}
