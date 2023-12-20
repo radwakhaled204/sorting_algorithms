79858598ab5e3164f4f2b7f957635743df80a725
Radwa-Khaled <radwaabouzed@gmail.com>
2023-12-19 17:37:57 -0800

    fix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,48 +1,46 @@
-
-
 #include "sort.h"
 
 /**
- * swap - swaps two elements in an array
- * @a: first element to swap
- * @b: second element to swap
+ * swap - swaps two elements in an array.
+ * @a: first element to swap.
+ * @b: second element to swap.
  */
+
 void swap(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
- * lomuto_partition - partitions an array using the Lomuto scheme
- * @array: the array to partition
- * @l-idx: the lowest index of the partition
- * @h-idx: the highest index of the partition
- * @size: the size of the array
- * Return: the final index of the pivot
+ * lomuto_partition - partitions an array using the Lomuto scheme.
+ * @array: the array to partition.
+ * @l_idx: the lowest index of the partition.
+ * @h_idx: the highest index of the partition.
+ * @size: the size of the array.
+ * Return: the final index of the par.
  */
-int lomuto_partition(int *array, int l-idx, int h-idx, size_t size)
+
+int lomuto_partition(int *array, int l_idx, int h_idx, size_t size)
 {
-    int pivot = array[h-idx];
-    int x = l-idx - 1;
+	int par = array[h_idx];
+	int x = l_idx - 1;
 
-    for (int y = l-idx; y < h-idx; y++)
-    {
-        if (array[y] <= pivot)
-        {
-            x++;
-            swap(&array[x], &array[y]);
-            if (x != y)
-                print_array(array, size);
-        }
-    }
-
-    swap(&array[x + 1], &array[h-idx]);
-    if (x + 1 != h-idx)
-        print_array(array, size);
-
-    return (x + 1);
+	for (int y = l_idx; y < h_idx; y++)
+	{
+		if (array[y] <= par)
+		{
+			x++;
+			swap(&array[x], &array[y]);
+			if (x != y)
+				print_array(array, size);
+		}
+	}
+	swap(&array[x + 1], &array[h_idx]);
+	if (x + 1 != h_idx)
+		print_array(array, size);
+	return (x + 1);
 }
 
 /**
@@ -53,27 +51,31 @@ int lomuto_partition(int *array, int l-idx, int h-idx, size_t size)
  * @h-idx: the highest index of the partition to sort
  * @size: the size of the array
  */
-void quick_sort_rec(int *array, int l-idx, int h-idx, size_t size)
-{
-    if (l-idx < h-idx)
-    {
-        int pi = lomuto_partition(array, l-idx, h-idx, size);
 
-        quick_sort_rec(array, l-idx, pi - 1, size);
-        quick_sort_rec(array, pi + 1, h-idx, size);
-    }
+void quick_sort_rec(int *array, int l_idx, int h_idx, size_t size)
+{
+	if (l_idx < h_idx)
+	{
+		int pi = lomuto_partition(array, l_idx, h_idx, size);
+
+		quick_sort_rec(array, l_idx, pi - 1, size);
+		quick_sort_rec(array, pi + 1, h_idx, size);
+	}
 }
 
 /**
  * quick_sort - sorts an array of integers in ascending
  * order using Quick sort algorithm
  * @array: the array to sort
+ * @l-idx: the lowest index of the partition.
+ * @h-idx: the highest index of the partition.
  * @size: the size of the array
+ *
  */
+
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
-
-    quick_sort_rec(array, 0, size - 1, size);
+	if (array == NULL || size < 2)
+		return;
+	quick_sort_rec(array, 0, size - 1, size);
 }
